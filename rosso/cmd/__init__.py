ca51fff4a5c9edff44efd60477d4937f90aaec70
Sho Minagawa <minagawa-sho@iij.ad.jp>
2018-06-23 12:44:44 +0900

    Support to parse RSS feeds that are recorded in file

--- rosso/cmd/__init__.py
+++ rosso/cmd/__init__.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 from rosso import __version__
@@ -17,34 +18,44 @@ def cli():
     pass
 
 
+def list_feed_entries(url_or_file, title, summary, pubdate):
+    feed = feedparser.parse(url_or_file)
+    for entry in feed.entries:
+        value = entry.title
+        if title == 'short' and len(value) > TITLE_SHORTEN_LENGTH:
+            value = value[:TITLE_SHORTEN_LENGTH] + '...'
+        click.echo(u'title: {}'.format(value))
+
+        value = entry.summary
+        # Sanitize the summary string which might include CRLFs
+        value = value.replace('\r', '').replace('\n', '')
+        if summary == 'short' and len(value) > SUMMARY_SHORTEN_LENGTH:
+            value = value[:SUMMARY_SHORTEN_LENGTH] + '...'
+        click.echo(u'summary: {}'.format(value))
+
+        value = entry.published
+        if pubdate:
+            value = time.strftime('%Y-%m-%d(%a) %H:%M:%S',
+                                  entry.published_parsed)
+        click.echo(u'pubDate: {}'.format(value))
+
+        # Insert a line padding between the entries
+        click.echo()
+
+
 @click.command()
 @click.option('--title', default='short', type=click.Choice(['full', 'short']))
 @click.option(
     '--summary', default='short', type=click.Choice(['full', 'short']))
 @click.option('--pubdate', default='ja', type=click.Choice(['raw', 'ja']))
-@click.argument('urls', nargs=-1)
-def list(title, summary, pubdate, urls):
-    for url in urls:
-        feed = feedparser.parse(url)
-        for entry in feed.entries:
-            value = entry.title
-            if title == 'short' and len(value) > TITLE_SHORTEN_LENGTH:
-                value = value[:TITLE_SHORTEN_LENGTH] + '...'
-            click.echo(u'title: {}'.format(value))
-
-            value = entry.summary.replace('\r', '').replace('\n', '')
-            if summary == 'short' and len(value) > SUMMARY_SHORTEN_LENGTH:
-                value = value[:SUMMARY_SHORTEN_LENGTH] + '...'
-            click.echo(u'summary: {}'.format(value))
-
-            value = entry.published
-            if pubdate:
-                value = time.strftime('%Y-%m-%d(%a) %H:%M:%S',
-                                      entry.published_parsed)
-            click.echo(u'pubDate: {}'.format(value))
-
-            # Insert a line padding between the entries
-            click.echo()
+@click.argument('url_or_filepaths', nargs=-1)
+def list(title, summary, pubdate, url_or_filepaths):
+    for url_or_filepath in url_or_filepaths:
+        if os.path.exists(url_or_filepath):
+            with open(url_or_filepath) as fp:
+                list_feed_entries(fp, title, summary, pubdate)
+        else:
+            list_feed_entries(url_or_filepath, title, summary, pubdate)
 
 
 cli.add_command(list)
